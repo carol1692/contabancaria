8d9d27815200b363e6b8ff3e5f635c94086738a1
Ana Lemos <carol1692@hotmail.com>
2025-09-21 07:45:59 -0300

    Inserção util cores, correção tabulação menu Sacar

--- conta_bancaria/src/conta_bancaria/Menu.java
+++ conta_bancaria/src/conta_bancaria/Menu.java
@@ -2,6 +2,8 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
+import conta_bancaria.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
@@ -10,30 +12,31 @@ public class Menu {
 		int opcao;
 						
 		while(true){
-			System.out.println("****************************************");
-			System.out.println("                                        ");
-			System.out.println("         BANCO DAS TCHUTCHUCAS          ");
-			System.out.println("                                        ");
-			System.out.println("****************************************");
-			System.out.println("                                        ");
-			System.out.println("   1 - Criar Conta                      ");
-			System.out.println("   2 - Listar todas as Contas           ");
-			System.out.println("   3 - Buscar Conta por Numero          ");
-			System.out.println("   4 - Atualizar Dados da Conta         ");
-			System.out.println("   5 - Apagar Conta                     ");
-			System.out.println("   6 - Sacar				            ");
-			System.out.println("   7 - Depositar                        ");
-			System.out.println("   8 - Transferir valores entre Contas  ");
-			System.out.println("   0 - Sair                             ");
-			System.out.println("                                        ");
-			System.out.println("****************************************");
-			System.out.println("Entre com a opção desejada:             ");
-			System.out.println("                                        ");
+			
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"****************************************	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"                                        	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"         BANCO DAS TCHUTCHUCAS          	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"                                        	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"****************************************	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"                                        	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   1 - Criar Conta                      	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   2 - Listar todas as Contas           	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   3 - Buscar Conta por Numero          	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   4 - Atualizar Dados da Conta         	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   5 - Apagar Conta                     	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   6 - Sacar					");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   7 - Depositar                        	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   8 - Transferir valores entre Contas  	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"   0 - Sair                             	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"                                        	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"****************************************	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"Entre com a opção desejada:             	");
+			System.out.println(Cores.TEXT_WHITE_BRIGHT + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT +"                                        	" + Cores.TEXT_RESET);
 			
 			opcao = input.nextInt();
 			
 			if(opcao == 0) {
-				System.out.println("\nBANCO DAS TCHUTCHUCAS - vem tchutchucas linda!!!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBANCO DAS TCHUTCHUCAS - vem tchutchucas lindas!!!");
 				sobre();
 				input.close();
 				System.exit(0);
@@ -41,31 +44,31 @@ public class Menu {
 			
 			switch(opcao) {
 				case 1:
-					System.out.println("Criar Conta \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta \n\n");
 					break;
 				case 2:
-					System.out.println("Listar todas as Contas");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas");
 					break;
 				case 3:
-					System.out.println("Buscar Conta por Numero \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Buscar Conta por Numero \n\n");
 					break;
 				case 4:
-					System.out.println("Atualizar Dados da Conta \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar Dados da Conta \n\n");
 					break;
 				case 5:
-					System.out.println("Apagar Conta \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar Conta \n\n");
 					break;
 				case 6:
-					System.out.println("Sacar \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Sacar \n\n");
 					break;
 				case 7:
-					System.out.println("Depositar \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Depositar \n\n");
 					break;
 				case 8:
-					System.out.println("Transferir valores entre Contas \n\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Transferir valores entre Contas \n\n");
 					break;
 				default:
-					System.out.println("\nOpção Inválida!\n");
+					System.out.println(Cores.TEXT_WHITE_BOLD + "\nOpção Inválida!\n");
 					break;
 					
 			}
